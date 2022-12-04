62cb3c5d17547c6e65d6f87e28f957a5b5e5c631
Shuhei Takahashi <shuheytakahashi@gmail.com>
2022-12-04 15:34:39 +0900

    main.js 設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -10,4 +12,21 @@ module.exports = {
     builder: "@storybook/builder-webpack5",
   },
   staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@balel/plugin-proposal-class-properties",
+      "@balel/plugin-proposal-private-methods",
+      "@balel/plugin-proposal-private-property-in-object",
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ];
+
+    return config;
+  },
 };
